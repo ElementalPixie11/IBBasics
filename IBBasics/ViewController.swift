9212e5bf65e4f394801b5dc42a4ec9a67f860cdc
Anasticia Johannsmeier <71769317+ElementalPixie11@users.noreply.github.com>
2021-04-14 10:01:20 -0400

    Initial Commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by Paige Johannsmeier on 4/14/21.
+//  Copyright © 2021 Anasticia Johannsmeier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
